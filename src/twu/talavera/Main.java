d86d1f939da7d991a591db7460542e2a6d9c5a2c
felipetv <ft.redprince@gmail.com>
2018-08-29 17:10:16 -0300

    Triangle Exercises

--- src/twu/talavera/Main.java
+++ src/twu/talavera/Main.java
@@ -1,8 +1,21 @@
 package twu.talavera;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Easiest exercise ever");
+        Triangle.printAsterisk();
+        System.out.println();
+
+        System.out.println("Draw a horizontal line");
+        Triangle.printAsteriskLine(8);
+        System.out.println();
+
+        System.out.println("Draw a vertical line");
+        Triangle.printVerticalAsteriskLine(3);
+        System.out.println();
+
+        System.out.println("Draw a right triangle");
+        Triangle.DrawRightTriangle(3);
     }
 }
---
+++ src/twu/talavera/Triangle.java
@@ -0,0 +1,27 @@
+package twu.talavera;
+
+class Triangle {
+    static void printVerticalAsteriskLine(int n) {
+        for (int i = 0; i < n; i++) {
+            printAsterisk();
+            System.out.println();
+        }
+    }
+
+    static void DrawRightTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            printAsteriskLine(i + 1);
+            System.out.println();
+        }
+    }
+
+    static void printAsteriskLine(int n) {
+        for (int i = 0; i < n; i++) {
+            printAsterisk();
+        }
+    }
+
+    static void printAsterisk() {
+        System.out.print("*");
+    }
+}
